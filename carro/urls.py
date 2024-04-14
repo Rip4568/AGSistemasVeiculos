2fbffa530d3fd432d03436f13e45b27d9e30ad6f
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-14 16:51:17 -0300

    adiciona rotas para funções

--- carro/urls.py
+++ carro/urls.py
@@ -6,4 +6,6 @@ app_name = 'carro'
 urlpatterns = [
     path('register_new_car/', views.register_new_car, name='register_new_car'),
     path('delete_car/<int:pk>/', views.delete_car, name='delete_car'),
+    path('update_car/<int:pk>/', views.update_car, name='update_car'),
+    path('render_form_update_car/<int:pk>/', views.render_form_update_car, name='render_form_update_car'),
 ]
