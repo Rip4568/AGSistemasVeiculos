3a5479fd1353af4c6626535963f9939b5e5eafa8
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-14 16:52:21 -0300

    requisições assincronas para para atualizar carro

---
+++ carro/static/js/index.js
@@ -0,0 +1,15 @@
+function getCookie(cookieName='csrftoken') {
+  window.document.cookie.split(';').forEach((cookie) => {
+    if (cookie.includes(cookieName)) {
+      return cookie.split('=')[1];
+    }
+  });
+}
+
+async function renderFormToUpdate(veiculoId) {
+  const modal = document.getElementById('edit_modal');
+  modal.show()
+  modal.querySelector('.content-form').innerHTML = 'Carregando...'
+  const response = await fetch(`/render_form_update_car/${veiculoId}/`);
+  modal.querySelector('.content-form').innerHTML = await response.text();
+}
