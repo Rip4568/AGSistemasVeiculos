4cfce88e3b26fc95e3497da782a0f1faa2875bca
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-14 15:33:19 -0300

    adiciona rotas de renderização de compoenntes

--- controle/urls.py
+++ controle/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('delete/<int:pk>/', views.delete_regisro_controle, name='delete'),
     path('update/<int:pk>/', views.update_regisro_controle, name='update'),
+    path('render_form_update/<int:pk>/', views.render_form_update_controle_registro, name='render_form_update'),
+    path('render_table_list/', views.render_table_list_controle_registro, name='render_table_list'),
 ]
