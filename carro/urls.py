b31d937fef97691087672a9e6ad865480d71655c
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-14 16:25:56 -0300

    Rota para deletação de carro

--- carro/urls.py
+++ carro/urls.py
@@ -5,4 +5,5 @@ app_name = 'carro'
 
 urlpatterns = [
     path('register_new_car/', views.register_new_car, name='register_new_car'),
+    path('delete_car/<int:pk>/', views.delete_car, name='delete_car'),
 ]
