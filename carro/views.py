9d8784882db754b0f85dfbf602849dbdaed75999
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-14 16:51:54 -0300

    correção no retorno das rotas, implementação de funções para deletar e renderizar pedaços de html

--- carro/views.py
+++ carro/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
+from django.template import loader
 
 from carro.forms import VeiculoForm
 from carro.models import Veiculo
@@ -20,4 +21,28 @@ def register_new_car(request:HttpRequest) -> HttpResponse:
 def delete_car(request:HttpRequest, pk:int) -> HttpResponse:
     veiculo = Veiculo.objects.get(pk=pk)
     veiculo.delete()
-    return redirect('motorista:register_and_list_motorista')
+    return redirect('carro:register_new_car')
+
+def update_car(request:HttpRequest, pk:int) -> HttpResponse:
+    veiculo = Veiculo.objects.get(pk=pk)
+    form = VeiculoForm(instance=veiculo)
+    if request.method == 'POST':
+        form = VeiculoForm(request.POST, instance=veiculo)
+        if form.is_valid():
+            form.save()
+    context = {
+        'form': form
+    }
+    return redirect('carro:register_new_car')
+
+
+def render_form_update_car(request:HttpRequest, pk:int) -> HttpResponse:
+    veiculo = Veiculo.objects.get(pk=pk)
+    form = VeiculoForm(instance=veiculo)
+    context = {
+        'form': form,
+        'veiculo': veiculo
+    }
+    return HttpResponse(
+        loader.render_to_string('carro/layouts/_form_car.html', context, request=request)
+    )
