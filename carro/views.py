b95d11ec9b1d1d77b9840db385b8189c3499b8ff
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-14 11:54:24 -0300

    implementação de função para listagem de veiculos e criação de novos

---
+++ carro/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from carro.forms import VeiculoForm
+from carro.models import Veiculo
+
+def home(request: HttpRequest) -> HttpResponse:
+    veiculos = Veiculo.objects.all()
+    context = {
+        'veiculos': veiculos
+    }
+    return render(request, 'carro/index.html', context)
+
+def register_new_car(request:HttpRequest) -> HttpResponse:
+    form = VeiculoForm
+    if request.method == 'POST':
+        form = VeiculoForm(request.POST)
+        if form.is_valid():
+            form.save()
+    veiculos = Veiculo.objects.all()
+    context = {
+        "form": form,
+        'veiculos': veiculos
+    }
+    return render(request, 'carro/register_car.html', context)
