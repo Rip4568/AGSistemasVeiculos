2df4bf30f3799f240375168429befe648e9862fc
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-14 15:34:39 -0300

    adiciona javascipt para implementar requisições ajax

--- controle/static/js/controle.js
+++ controle/static/js/controle.js
@@ -1,15 +1,65 @@
-function renderUpdateForm(registroId) {
-  fetch(`/controle/update/${registroId}/`)
-    .then(response => response.text())
-    .then(html => {
-      // Encontre o elemento do modal e substitua o conteúdo interno
-      const editModal = document.getElementById(`edit_modal_${registroId}`);
-      editModal.innerHTML = html;
+async function renderUpdateForm(registroId) {
+  try {
+    const response = await fetch(`/render_form_update/${registroId}/`);
+    const html = await response.text();
 
-      // Exiba o modal
-      editModal.showModal();
-    })
-    .catch(error => {
-      console.error('Erro ao renderizar o formulário de atualização:', error);
-    });
+    // Encontre o elemento do modal e substitua o conteúdo interno
+    const editModal = document.getElementById(`edit_modal`);
+    const contentForm = editModal.querySelector('.content-form');
+    contentForm.innerHTML = html;
+
+    // Exiba o modal
+    editModal.show();
+  } catch (error) {
+    console.log('Erro ao renderizar o formulário de atualização:', error);
+  }
 }
+
+function addListnerFormSubmit() {
+  const form = document.getElementById('filtrar-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderTableRegister(form);
+  })
+}
+
+async function renderTableRegister(form) {
+  const data = Object.fromEntries(new FormData(form));
+  console.log(data);
+  const response = await fetch('/render_table_list/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': getCookie('csrftoken')
+    },
+    body: JSON.stringify({data})
+  });
+  const html = await response.text();
+  const tableListControle = document.getElementById('table-list-controle');
+  tableListControle.innerHTML = html;
+}
+
+function getCookie(cookieName) {
+  const name = cookieName + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+  for(let i = 0; i < cookieArray.length; i++) {
+      let cookie = cookieArray[i];
+      while (cookie.charAt(0) === ' ') {
+          cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(name) === 0) {
+          return cookie.substring(name.length, cookie.length);
+      }
+  }
+  return "";
+}
+
+function getCookieSimple(cookieName='csrftoken') {
+  window.document.cookie.split(';').forEach((cookie) => {
+    if (cookie.includes(cookieName)) {
+      return cookie.split('=')[1];
+    }
+  });
+}
+addListnerFormSubmit()
