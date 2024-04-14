091df718fb812095f794a7ca87f1616e377fcbc6
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-14 15:33:48 -0300

    implementaa função de renderizção de de componentes parciais

--- controle/views.py
+++ controle/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.template import loader
@@ -44,16 +45,37 @@ def update_regisro_controle(request: HttpRequest, pk: int) -> HttpResponse:
     context = {
         'form': form
     }
-    return render(request, 'controle/index.html', context)
+    return redirect('controle:home')
 
 def render_form_update_controle_registro(request: HttpRequest, pk: int) -> HttpResponse:
     controle = ControleRegistro.objects.get(pk=pk)
     form = ControleRegistroForm(instance=controle)
     context = {
-        'form': form
+        'form': form,
+        'registro': controle
     }
-    return loader.render_to_string(
+    return HttpResponse(loader.render_to_string(
         'controle/layouts/_form_controle.html',
         context,
-        request=request
-    )
+        request=request))
+    
+    
+def render_table_list_controle_registro(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        filtrar_data_registro = data.get('filtrar-data-registro')
+        
+        registros = ControleRegistro.objects.filter(
+            saida__icontains=filtrar_data_registro
+        ).select_related(
+            'veiculo', 
+            'motorista'
+        ).order_by('-saida')
+        context = {
+            'registros': registros
+        }
+        return HttpResponse(loader.render_to_string(
+            'controle/layouts/_table_list_controle.html',
+            context,
+            request=request))
+    return HttpResponse('Metodo não aceito')
